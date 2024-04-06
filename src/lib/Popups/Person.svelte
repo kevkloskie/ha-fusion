39bfbc8f07df776b16dde4ce7a2c4a50ed949971
kevkloskie <36130631+kevkloskie@users.noreply.github.com>
2024-04-06 12:10:04 +0200

    Create Person.svelte

---
+++ src/lib/Popups/Person.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { states, lang } from '$lib/Stores';
+	import Modal from '$lib/Modal/Index.svelte';
+	import StateLogic from '$lib/Components/StateLogic.svelte';
+	import { getName } from '$lib/Utils';
+	import Bar from '$lib/Sidebar/Bar.svelte';
+	import Graph from '$lib/Sidebar/Graph.svelte';
+	import Device from '$lib/Modal/DeviceTrackerModal.svelte';
+
+	export let isOpen: boolean;
+	export let sel: any;
+
+	$: entity = $states[sel?.entity_id];
+</script>
+
+{#if isOpen}
+	<Modal>
+		<h1 slot="title">{getName(sel, entity)}</h1>
+		<StateLogic entity_id='person.stephan' />
+	</Modal>
+{/if}
+
+<style>
+
+</style>
